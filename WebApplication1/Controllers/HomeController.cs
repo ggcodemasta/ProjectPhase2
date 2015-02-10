b8189ca4068742071d3c08599afafc6651c11cb9
ggcodemasta <grant_yao@live.ca>
2015-02-10 10:18:41 -0800

    Grant's test commit

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-
+        //Grant test commit
         public ActionResult Index()
         {
             CareerProfileRepository careerProfileRepository = new CareerProfileRepository();
