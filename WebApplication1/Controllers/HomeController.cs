ced57ffca12a2a8bec57f5507e5679656533d137
ggcodemasta <grant_yao@live.ca>
2015-02-10 10:40:25 -0800

    steve/grant conflict merge

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -12,9 +12,6 @@ namespace WebApplication1.Controllers
     {
         //Grant test commit
 
-
-
-
         public ActionResult Index()
         {
             CareerProfileRepository careerProfileRepository = new CareerProfileRepository();
