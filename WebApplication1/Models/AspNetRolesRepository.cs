e320639abf90b428bddeb7ff8cdaaf4dbdab7377
calmwoods <calmwoods@gmail.com>
2015-02-23 18:58:00 -0800

    Sorry. Adding AspNetRolesRepository.cs

---
+++ WebApplication1/Models/AspNetRolesRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class AspNetRolesRepository
+    {
+        public int AddOneRole(AspNetRole role)
+        {
+            EmployeesEntities db = new EmployeesEntities();
+            db.AspNetRoles.Add(role);
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return -1; 
+            }
+            return 1; 
+        }
+        
+        public IEnumerable<AspNetRole> GetAllAspNetRoles()
+        {
+            EmployeesEntities db = new EmployeesEntities();
+            return db.AspNetRoles.ToList();
+        }
+    }
+}
